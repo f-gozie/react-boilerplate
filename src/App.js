4a21a5ad298dc9ff6e9eef7fbae1f8a8e71344b7
Agozie Favour <fagozie43@gmail.com>
2020-12-02 20:20:50 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+import CounterExample from './components/CounterExample';
+
+function App() {
+  return (
+    <div>
+        <CounterExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CounterExample.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+function CounterExample(props) {
+
+	const [count, setCount] = useState(0);
+
+	return (
+		<div>
+			
+		</div>
+	);
+}
+
+export default CounterExample;
